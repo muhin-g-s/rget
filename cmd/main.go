059ca8c2c4fa53f926147dfc887128049196bb07
Мухин Григорий <muhin.g.s.00@gmail.com>
2026-03-11 10:13:01 +0300

    test

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,22 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/muhin-g-s/rget/internal/app"
 )
 
+func CountWords(text string) int {
+	words := strings.Fields(text)
+	return len(words)
+}
+
 func main() {
+	text := "Go is a simple and powerful language"
+	count := CountWords(text)
+
+	fmt.Println("Word count:", count)
+
 	result := app.Run(os.Args[1:])
 
 	fmt.Printf("%s", result)
